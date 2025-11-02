8c6eb7746116d13af0d3cc977707bef89527d1c8
Omar Naguib <omar.naguib2110@gmail.com>
2025-11-02 22:39:32 +0200

    create the get_computer_choice function in the JS file

---
+++ javascript.js
@@ -0,0 +1,13 @@
+//return random string "rock" / paper /scissors
+function get_computer_choice() {
+  // get a random value from 1 to 3
+  let r_0to99 = parseInt(Math.random() * 100);
+  // console.log(r_0to99);
+  // associate every number to a string
+  // return the string
+  if (r_0to99 >= 0 && r_0to99 < 33) return "rock";
+  if (r_0to99 >= 33 && r_0to99 <= 66) return "paper";
+  if (r_0to99 > 66 && r_0to99 <= 99) return "scissors";
+}
+
+console.log(get_computer_choice());
